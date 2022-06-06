af879e18c4005c49460dd95a521d259a05b8ae59
Macasore <macasorekingdavid@gmail.com>
2022-06-06 03:59:46 -0700

    print the size of various variables on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-
-/**
+i/**
  * main - Entry point
  *
  * Return: Return always 0 (success)
@@ -9,14 +8,14 @@ int main(void)
 {
 	char c;
 	int i;
-	long int li;
-	long long int lli;
+	long int i1;
+	long long int i2;
 	float f;
 	
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli);
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i1));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i2);
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
