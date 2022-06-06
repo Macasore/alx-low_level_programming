f452a23d00b598dc4b70c900c41522eecae6c0a6
Macasore <macasorekingdavid@gmail.com>
2022-06-06 03:20:08 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 /** 
- * this is the main function
- * return: always 0 (success)
+ * main - This is the main function
+ * Return: Always 0 (success)
+ *
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
-	}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	
+}
