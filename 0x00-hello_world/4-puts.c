227fea5d1629afd2b43f147edb8a1675e4a48518
Macasore <macasorekingdavid@gmail.com>
2022-06-06 03:13:36 -0700

    added a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/** 
+ * this is the main function
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n"
+	return(0)
+	}
