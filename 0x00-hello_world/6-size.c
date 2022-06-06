f40ee9553fedfede69abc85ecdb6aceb294f2f2b
Macasore <macasorekingdavid@gmail.com>
2022-06-06 04:08:09 -0700

    print the size of various variables on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,6 @@ int main(void)
 	long int i1;
 	long long int i2;
 	float f;
-	
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i1));
