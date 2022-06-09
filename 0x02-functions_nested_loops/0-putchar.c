1384ca8bc254e21cfa366d0a88fdda6d73098137
Macasore <macasorekingdavid@gmail.com>
2022-06-08 17:19:41 -0700

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  *  * main - prints to the console _putchar followed by a newline
  *   * Return: - Always 0 (success)
  *    */
-int main(void);
+int main(void)
 {
 	_putchar("_");
 	_putchar("p");
