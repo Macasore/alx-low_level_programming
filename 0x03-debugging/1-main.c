2f5ec48a14d37c458f3e846d73978327efd2f5c5
Macasore <macasorekingdavid@gmail.com>
2022-06-10 16:40:36 -0700

    fixed a bug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ int main(void)
 
 	i - 0;
 	
-/**	while (i < 10)
-	{
-		putchar(i);
-	}
-*/	printf("Infinite loop avoided! \\0/\n");
+/**	while (i < 10)*/
+	/*{*/
+	      /*putchar(i);*/
+      /*}*/
+	printf("Infinite loop avoided! \\0/\n");
 
 	return (0);
 }
