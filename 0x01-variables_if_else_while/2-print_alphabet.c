a223d433ce17d3cf79d67f0cec2359ca67fa1467
Macasore <macasorekingdavid@gmail.com>
2022-07-25 16:12:48 -0700

    prints all the alphabets in small letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return : always 0 (success)
+ */
+
+int main()
+{
+	char sml;
+	for (sml = 'a'; sml <= 'z'; sml++)
+	{
+		putchar(sml);
+	}
+	putchar("\n");
+	return(0);
+}
+
