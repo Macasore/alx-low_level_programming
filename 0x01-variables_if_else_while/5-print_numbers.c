50a22686c5a339fde0f164a3fce13960a2c2c141
Macasore <macasorekingdavid@gmail.com>
2022-07-27 02:57:56 -0700

    prints all the single digits in base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Return Always 0 (success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit);
+	}
+	putchar("\n");
+	return (0);
+}
+
