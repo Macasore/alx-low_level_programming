d113dcb92141b675cdf32cd79d97828a6a32d8e2
Macasore <macasorekingdavid@gmail.com>
2022-07-27 06:04:36 -0700

    prints numbers while seperating them with ,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Return Always 0 (success)
+ */
+
+int main(void)
+{
+	char comb;
+
+	for (comb = '0'; comb <= '9'; comb++)
+	{
+		putchar(comb);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
