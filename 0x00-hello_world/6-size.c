908af2edb1c1c7fb52c743479170de04ab0128ed
Macasore <macasorekingdavid@gmail.com>
2022-12-11 16:28:45 +0100

    prints the sizes of various datatype on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	int i;
 	char c;
 	float f;
-	long long j;
+	long long int j;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
