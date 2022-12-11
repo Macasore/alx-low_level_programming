68556ebf66d3bf7525716e179ec7b883b5534d48
Macasore <macasorekingdavid@gmail.com>
2022-12-11 14:09:04 +0100

    prints a string with printf followed by a newline

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints a string with the printf function followed by a mew line"
+*
+*Return: returns 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
