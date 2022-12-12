a63dff83c4438762f4739da315cbdd98ca9c57b6
Macasore <macasorekingdavid@gmail.com>
2022-12-12 01:56:45 +0100

    prints all the alphabets in lowercase without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - prints the letters from a to z except q and e followed by a newline.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	char first;
+
+	for (first = 'a'; first <= 'z'; first++)
+	{
+		if (first == 'e' || first == 'q'){
+			continue;
+		}
+		putchar(first);
+	}
+	putchar('\n');
+	return (0);
+}
