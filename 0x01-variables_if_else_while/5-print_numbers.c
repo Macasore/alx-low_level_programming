21ef28a7903bf3616fc5ade83eea2ddeea4a791a
Macasore <macasorekingdavid@gmail.com>
2022-12-12 02:03:29 +0100

    prints all single digit numbers of base10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers of base10 starting from 0
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+
+	while (i != 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
