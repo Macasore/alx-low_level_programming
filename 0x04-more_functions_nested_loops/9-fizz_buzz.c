00aab564ae839b28ffe3bf4c8d5b036b12658c68
Macasore <macasorekingdavid@gmail.com>
2022-12-16 01:49:08 +0100

    prints from 1 to 100, with some extra twist

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0.
 */
-void main(void)
+int main(void)
 {
 	int i;
 
@@ -14,17 +14,14 @@ void main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
-			continue;
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
-			continue;
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-			continue;
 		}
 		else
 		{
@@ -32,4 +29,6 @@ void main(void)
 		}
 	}
 	printf("\n");
+
+	return (0);
 }
