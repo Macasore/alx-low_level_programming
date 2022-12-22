4b60d9261d8b121563a9c6f75451321d80485d1b
Macasore <macasorekingdavid@gmail.com>
2022-12-22 02:58:58 +0100

    concatenates strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,27 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * *_strncat - concatenates two strings.
- * @dest: the string to be appended to.
- * @src: the string to append.
- * @n: the allowed number of bytes for src to append.
+ * _strncat - concatinating n bytes from source string with
+ * destination string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be concatenated
  *
- * Return: Always 0 success.
-*/
+ * Return: pointer string
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int start = strlen(dest);
 	int i = 0;
-	int j = 0;
 
-	while (dest[i] != '\0')
+	while (i < n)
 	{
+		dest[start + i] = src[i];
 		i++;
 	}
-	while (j < n)
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
+
+
 	return (dest);
 }
