745895f271c3f246f8e604c130aa7d2448e25b9c
Macasore <macasorekingdavid@gmail.com>
2022-12-22 13:38:07 +0100

    copies strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
-
+	while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
 	return (dest);
+
 }
