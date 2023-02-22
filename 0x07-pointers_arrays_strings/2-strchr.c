398f323f22b67c27132ee94d0c590ad76e3bd382
Macasore <macasorekingdavid@gmail.com>
2023-02-22 18:31:50 +0100

    testing script

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - this function locates a chracter in a string
+ * @s: the string to be checked
+ * @c: the character to be looked for
+ *
+ * Return: returns a pointer to the first occurence of the character
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (s);
+}
