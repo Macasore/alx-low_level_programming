83b391d8107f1344a46867e735bf89051dd413ab
Macasore <macasorekingdavid@gmail.com>
2023-02-24 01:37:35 +0100

    create _strstr function in c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,63 +1,35 @@
 #include "main.h"
-#include <stddef.h>
-#include <string.h>
-
-int contains(char *s, char *c);
-
 /**
- * _strstr - finds the substring of a char c
- * @haystack: string
- * @needle: substring
- *
- * Return: pointer to substring
+ *_strstr - find a string in a string.
+ *@haystack: string.
+ *@needle: substring.
+ *Return: char.
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-	char *p;
+	unsigned int length_needle = 0, i = 0, j, tmpi, checker;
 
-	i = 0;
+	while (needle[length_needle] != '\0')
+		length_needle++;
 
 	while (haystack[i] != '\0')
 	{
-		p = haystack + i;
-
-		if (contains(p, needle))
-			return (p);
-
+		tmpi = i;
+		checker = 0;
+		for (j = 0; j < length_needle; j++)
+		{
+			if (haystack[tmpi] == needle[j])
+			{
+				tmpi++;
+				checker++;
+			}
+		}
+		if (checker == length_needle)
+			return (haystack + i);
 		i++;
 	}
 
-	return (NULL);
-}
-
-/**
- * contains - checks if c is a substring of s
- * @s: string
- * @c: substring
- *
- * Return: pointers to substring of c or NULL if none
- */
-int contains(char *s, char *c)
-{
-	int i, j;
-
-	i = 0;
-	j = 0;
-	while (s[i] != '\0' && c[i] != '\0')
-	{
-
-		if (s[i] != c[i])
-			return (0);
-
-		j++;
-		i++;
-	}
-
-	if (j != (int) strlen(c))
-		return (0);
-
-	return (1);
+	return (0);
 
 }
