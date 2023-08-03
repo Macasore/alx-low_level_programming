d3808ab24ee3a02deee54431dba2d993103a727b
root <root@DESKTOP-F38M3DK>
2023-08-03 01:18:55 +0100

    feat:adds a function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a newline
- * @s: the string to be printed
- *
- * Return: Always 0
+* _puts_recursion - a function that prints a string followed by a new line
+* @s: the operand to be printed
+*
+* Return: always Zero
 */
 
 void _puts_recursion(char *s)
 {
-
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
-	s++;
-	_puts_recursion(s);
-
+	_puts_recursion(s + 1);
 }
