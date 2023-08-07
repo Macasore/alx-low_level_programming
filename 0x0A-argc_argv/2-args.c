b7255d4d7bdb9bff70de54d6aa544727361f0fd4
root <root@DESKTOP-F38M3DK>
2023-08-07 20:20:26 +0100

    a program that prints all the arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,18 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
- * main - prints all the arguments it receives
- * @argc: the argument counter
- * @argv: the argument vector
- *
- * Return: 0 or 1 depending on the result
+* main - the entering function
+* @argc: the argument counter
+* @argv: the argument vector
+* Return: always zero
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int count;
+	int i;
 
-	for (count = 0; count < argc; count++)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[count]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
