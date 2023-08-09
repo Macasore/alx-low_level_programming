5c17e4247d5561fb1cf1b1de7aebb26384654560
root <root@DESKTOP-F38M3DK>
2023-08-09 05:02:50 +0100

    returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+int count_character(char *s);
+
+/**
+* _strdup - returns a pointer to a newly allocated space in memory
+* @str: the string
+* Return: the pointer or null
+*/
+
+char *_strdup(char *str)
+{
+	char *nw_store;
+	int count = 0;
+
+	if (str == NULL)
+		return (NULL);
+	count = count_character(str);
+	nw_store = malloc(sizeof(char) * count);
+
+	if (nw_store == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < count; i++)
+		{
+			nw_store[i] = str[i];
+		}
+	}
+	return (nw_store);
+}
+
+/**
+* count_character - this is used to count the number of characters
+* @s: the character to be counted
+* Return: the count
+*/
+
+int count_character(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count += 1;
+		s++;
+	}
+	return (count);
+}
