f8f9a12d24fe5da7bf99885d7ec339c1f4c2c101
root <root@DESKTOP-F38M3DK>
2023-08-10 19:25:29 +0100

    concatenates two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdlib.h>
+int counter(char *s);
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: the size of the second string required
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int count1, count2, i, j, total;
+	char *nw_space;
+
+	count1 = counter(s1);
+	count2 = counter(s2);
+	total = count1 + count2 + 1;
+
+	nw_space = malloc(sizeof(char) * total);
+
+	if (nw_space == NULL)
+		return (NULL);
+
+	for (i = 0; i < count1; i++)
+	{
+		nw_space[i] = s1[i];
+	}
+	for (j = 0; j < count2; j++)
+	{
+		if ((int)n < count2)
+		{
+			if (n == 0)
+				break;
+			nw_space[i + j] = s2[j];
+			n--;
+		}
+		nw_space[i + j] = s2[j];
+	}
+	nw_space[i + j] = '\0';
+
+	return (nw_space);
+}
+
+/**
+* counter - this function counts the string characters.
+* @s: the string to be counted
+* Return: returns the count
+*/
+
+int counter(char *s)
+{
+	int count = 0;
+
+	if (s == NULL)
+		return (count);
+	while (*s != '\0')
+	{
+		count += 1;
+		s++;
+	}
+	return (count);
+}
