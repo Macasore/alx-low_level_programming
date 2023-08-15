0e51ea907dbfb353423b4b8630fb20aa347ebf1e
root <root@DESKTOP-F38M3DK>
2023-08-15 04:54:45 +0100

    define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef _DOG_H
 #define _DOG_H
-
+/**
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: owner's name
+*/
 struct dog
 {
 	char *name;
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -2,4 +2,5 @@
 #define _MAIN_H
 
 void _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
