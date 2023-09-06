c1133084ba5485f62a2321c8dd9188a36b75c88a
root <root@DESKTOP-F38M3DK>
2023-09-06 22:14:14 +0100

    appends a text to the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_WRONLY | O_APPEND, 0);
+	fd = open(filename, O_WRONLY | O_APPEND, 0664);
 
 	if (fd == -1)
 		return (-1);
