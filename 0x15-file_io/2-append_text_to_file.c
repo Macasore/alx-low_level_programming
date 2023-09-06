fdf9a08969f94e8f173bded1fff83e095dcc3600
root <root@DESKTOP-F38M3DK>
2023-09-06 22:06:05 +0100

    appends a text to the end of a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,53 @@
+#include "main.h"
+int count(char *text);
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the file where the text would be appended
+ * @text_content: the string to be appended
+ * Return: 1 on success -1 on failure
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, length;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		length = count(text_content);
+		bytes_written = write(fd, text_content, length);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
+
+/**
+ * count - counts the length of the string
+ * @text: the string to be counted
+ * Return: returns the length of the string
+*/
+
+int count(char *text)
+{
+	int counter = 0;
+
+	while (*text != '\0')
+	{
+		counter++;
+		text++;
+	}
+	return (counter);
+}
