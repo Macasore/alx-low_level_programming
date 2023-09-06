f178179f7562253515147fc1ac7bebe300cabbd3
root <root@DESKTOP-F38M3DK>
2023-09-06 22:10:43 +0100

    appends a text to the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -31,6 +31,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 	}
 	close(fd);
+	if (bytes_written < 0)
+        return (-1);
+    else
+        return 1;
+
 	return (1);
 }
 
